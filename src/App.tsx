38e28ad8963a1ce6455ea682161ab46f35dd1165
J M Pramod <pramod@ctinfopro.com>
2023-08-01 09:14:52 +0530

    route

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,59 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.css';import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/home",
+      element: <div>home</div>,
+    },
+    {
+      path: "/",
+      element: <div>no home</div>,
+    },
+    {
+      path: "/about",
+      element: <div>about</div>,
+    },
+    {
+      path: "/event/149",
+      element: <div>event/149</div>,
+    },
+    {
+      path: "/giveaways",
+      element: <div>giveaways</div>,
+    },
+    {
+      path: "/live",
+      element: <div>live</div>,
+    },
+    {
+      path: "/login",
+      element: <div>login</div>,
+    },
+    {
+      path: "/register",
+      element: <div>/register</div>,
+    },
+    {
+      path: "/past/116",
+      element: <div>past/116</div>,
+    },
+  
+
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ <div>
+   <RouterProvider router={router} />
+ </div>
   );
 }
 
